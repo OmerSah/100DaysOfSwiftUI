28eb27365d71340f39ae8164372ae76829f72e56
Ömer Faruk Şahin <omersahin@Omers-MacBook-Air.local>
2021-05-22 23:28:16 +0300

    Practise array, sets, tuples and dictionaries

---
+++ Day 2/Second Day.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Collections
+
+let arr = ["o","m","e","r"]
+
+print(arr[0]+arr[1]+arr[2]+arr[3])
+
+// Set: Not ordered and no duplicates like hash set
+let set = Set(["one","two","three"])
+
+var tuple = (first: "Omer", second: "Sahin")
+tuple.0 // Omer
+tuple.first // Omer
+tuple.first = "Faruk" // Can be assigned a new value in same type
+
+// Dictionaries
+var ages = ["omer": 22, "faruk": 23]
+ages["omer"] // 22
+
+
