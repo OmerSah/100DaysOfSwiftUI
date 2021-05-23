825095f05d4f2e39294928deb177057b386e21e8
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-05-23 22:28:26 +0300

    Learn basic operators in Swift

---
+++ Day 3/Third Day.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// Operators and Conditions
+
+var first = 3
+var second = 2
+var result = 3 * 2
+
+result % (first + second) // 1
+
+let str1 = "Omer "
+let str2 = "Faruk"
+var str3 = str1 + str2 // Omer Faruk
+// Swift is type-safe language(You can not mix the types)
+
+// Compound assignment operators
+first += 5 // 8
+str3 += str1 // "Omer FarukOmer"
+
+// Comparison operators
+first == second // false
+first >= second // true
+str1 > str2 // true
