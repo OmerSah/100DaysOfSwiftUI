819f3f543d0f9b553f17bf1653b76e3ef937995d
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-05-23 23:06:24 +0300

    Add conditions and switch case and ternary, range and bitwise operators

--- Day 3/Third Day.playground/Contents.swift
+++ Day 3/Third Day.playground/Contents.swift
@@ -21,3 +21,42 @@ str3 += str1 // "Omer FarukOmer"
 first == second // false
 first >= second // true
 str1 > str2 // true
+
+// Conditions
+if first + second == result {
+    print(1)
+} else if first + second == 10 {
+    print(2) // Prints 2
+} else {
+    print(3)
+}
+
+// Bitwise Operators
+print(first > result && first > second) // Prints true
+
+// Ternary Operator
+print(first > second ? "Ternary" : "Operator") // Ternary
+
+// Switch Case
+let Mood = "Happy"
+
+switch Mood {
+case "Happy":
+    print("I am happy")
+    fallthrough // Continue
+default:
+    print("I don't know")
+} // Print both two cases duue to fallthrough
+
+// Range operators
+let numberOfPeople = 50
+
+switch numberOfPeople {
+case 0 ... 25: // Between 0 and 25 (Both of are included)
+    print("Too small")
+case 25 ..< 50: // 50 is excluded
+    print("It is OK")
+default:
+    print("It is crowded") // Prints
+}
+
