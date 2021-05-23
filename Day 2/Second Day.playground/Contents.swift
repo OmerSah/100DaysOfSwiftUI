1d992c85cd7be2b34ddcd375e3f3b1f60e1de3db
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-05-23 21:50:35 +0300

    Update enums with associated and raw values

--- Day 2/Second Day.playground/Contents.swift
+++ Day 2/Second Day.playground/Contents.swift
@@ -8,6 +8,7 @@ print(arr[0]+arr[1]+arr[2]+arr[3])
 
 // Set: Not ordered and no duplicates like hash set
 let set = Set(["one","two","three"])
+set.contains("one")
 
 var tuple = (first: "Omer", second: "Sahin")
 tuple.0 // Omer
@@ -22,13 +23,26 @@ ages["omer"] // 22
 var array = [Int]() // Empty array
 var dict = [String: String]() // Empty dictionary
 dict["a"] = "b" // Adds a new element
+var newArray = Array<Int>()
+var newDict = Dictionary<String,String>()
 
 // Type should be written in set
 var newSet = Set<Int>() // Empty set
 
-// Enums
+// Enums with associated values
 enum State {
-    case Opened
-    case Closed
+    case Opened(time: String)
+    case Closed(time: String)
 }
+
+let state = State.Opened(time: "11.30")
+
+// Enums with raw values
+enum Sport: Int{
+    case football
+    case volleyball
+    case basketball
+}
+
+let sport = Sport(rawValue: 2) // Basketball
 
