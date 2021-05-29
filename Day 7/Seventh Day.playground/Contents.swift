5523769cc0fb4cfbda5bde17263e37f86d15b515
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-05-29 23:22:04 +0300

    Add closures as parameters with return

--- Day 7/Seventh Day.playground/Contents.swift
+++ Day 7/Seventh Day.playground/Contents.swift
@@ -12,3 +12,15 @@ func goTo(action: (String) -> Void) {
 goTo { (destination: String) in
     print("You are going to \(destination)") // Second
 } // Prints First + Second
+
+func whoAreYou(_ name: String, action: (String) -> (String)) {
+    print("Who are you?")
+    let answer = action(name)
+    print(answer)
+}
+
+// Trailing closure syntax
+whoAreYou("Omer") {(name: String) -> String in
+    return "Hey, I am \(name)"
+} // Who are you? Hey, I am Omer
+
