aedff32efda49989f18f940e1dac623719383691
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-05-30 22:56:23 +0300

    Add a struct with computed property

--- Day 8/Eighth Day.playground/Contents.swift
+++ Day 8/Eighth Day.playground/Contents.swift
@@ -14,3 +14,20 @@ print(student.age, student.id) // 20 1
 
 student.age = 21
 print(student.age, student.id) // 21 1
+
+// Struct with computed property
+struct User {
+    let name: String
+    let isItAdmin: Bool
+    // Computed properties
+    var authorityStatus: String {
+        if isItAdmin {
+            return "\(name) is an admin."
+        } else {
+            return "\(name) is not an admin."
+        }
+    }
+}
+
+var user = User(name: "Omer", isItAdmin: true)
+print(user.authorityStatus) // Omer is an admin
