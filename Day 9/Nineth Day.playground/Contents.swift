3b602efed747ec9b2798154479205d232ce1c60e
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-05-31 21:22:24 +0300

    Control the access to properties and methods

--- Day 9/Nineth Day.playground/Contents.swift
+++ Day 9/Nineth Day.playground/Contents.swift
@@ -40,18 +40,22 @@ struct CSStudent {
 var csStudent = CSStudent(name: "Omer") // Become an iOS Developer Omer
 csStudent.workExperience // Loading...
 
-// Static properties
+// Static and private properties
 struct Student {
     static var numberOfStudents: Int = 0
     var name: String = "Omer"
+    // You can not access to id directly
+    private let id: Int
     // Every instance share same numberOfStudents
-    init(name: String) {
+    init(name: String, id: Int) {
         self.name = name
         Student.numberOfStudents += 1
+        self.id = id
+        print("Your id is \(id)")
     }
 }
 
-var s1 = Student(name: "Omer")
-var s2 = Student(name: "Faruk")
-var s3 = Student(name: "Sahin")
+var s1 = Student(name: "Omer",id: 1) // Your id is 1
+var s2 = Student(name: "Faruk",id: 2) // Your id is 2
+var s3 = Student(name: "Sahin",id: 3) // Your id is 3
 print(Student.numberOfStudents) // 3
