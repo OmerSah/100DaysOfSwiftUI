438a34d550c25b202f0fef32e302ea765b421129
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-07-25 17:53:13 +0300

    Use views as properties

--- Day15-30/Day23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Day15-30/Day23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,24 +10,32 @@ import SwiftUI
 struct ContentView: View {
     @State private var isBlue = false
     
+    var button: some View {
+        Button("Hello!") {
+            print(type(of: self.body))
+            self.isBlue.toggle()
+        }
+    }
+    
+    let btn1 = Text("Welcome 1")
+    let btn2 = Text("Welcome 2")
     var body: some View {
         VStack {
             VStack {
-                Text("Welcome")
+                    btn1
                     .font(.largeTitle)
-                Text("Welcome")
+                    btn2
                     // This does not change the text view because blur is a regular modifier
                     .blur(radius: 0)
-                Text("Welcome")
-                Text("Welcome")
+                Text("Welcome 3")
+                Text("Welcome 4")
             } // font modifier is an environment modifier and we can override it with a custom font
             .font(.title)
             .blur(radius: 5)
             
-            Button("Hello!") {
-                print(type(of: self.body))
-                self.isBlue.toggle()
-            } // Use ternary operator to use conditional modifiers
+            // Use computed property to hold views as properties
+            button
+            // Use ternary operator to use conditional modifiers
             .foregroundColor(isBlue ? Color.blue : Color.white)
             .padding()
             .background(Color.red)
