6cd051a3d177278efff38c8e7592f56fda1f4d51
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-07-26 23:55:19 +0300

    Complete the challenge

---
+++ Day15-30/Day24/Challenge/Challenge/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Ömer Faruk Şahin on 26.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day15-30/Day24/Challenge/Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge
+//
+//  Created by Ömer Faruk Şahin on 26.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
