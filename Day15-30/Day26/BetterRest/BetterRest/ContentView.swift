a14c231ff97d74eef4678b856c8a950e437376af
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2021-08-01 18:12:29 +0300

    Add different type of DatePicker views

--- Day15-30/Day26/BetterRest/BetterRest/ContentView.swift
+++ Day15-30/Day26/BetterRest/BetterRest/ContentView.swift
@@ -9,11 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepingHours = 8.0
+    @State private var wakeUp = Date()
+    @State private var randomDay = Date()
+    @State private var secondRandDay = Date()
     
     var body: some View {
-        Stepper(value: $sleepingHours, in: 4...12, step: 0.25, label: {
-            Text("\(sleepingHours, specifier: "%g") hours")
-        })
+        // Set it to the current date
+        let now = Date()
+        // 86400 : 1 day in seconds
+        let tomorrow = Date().addingTimeInterval(86400)
+        let range = now ... tomorrow
+        
+        VStack {
+            Stepper(value: $sleepingHours, in: 4...12, step: 0.25, label: {
+                Text("\(sleepingHours, specifier: "%g") hours")
+            })
+            
+            // Between today and tomorrow
+            DatePicker("Now to tomorrow", selection: $randomDay, in: range)
+            // From now to future not past
+            DatePicker("Now to future", selection: $secondRandDay, in: Date()...)
+            
+            DatePicker("Select the hour", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+        }
     }
 }
 
